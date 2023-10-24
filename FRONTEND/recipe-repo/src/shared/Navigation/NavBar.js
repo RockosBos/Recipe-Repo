1e25b428341a73d6353f6d7f431bdac2ce49c4f8
RockosBos <32206608+RockosBos@users.noreply.github.com>
2023-10-24 19:50:05 -0400

    formatting changes

--- FRONTEND/recipe-repo/src/shared/Navigation/NavBar.js
+++ FRONTEND/recipe-repo/src/shared/Navigation/NavBar.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
+import {Nav} from '../../components/NavComponents'
 
 export default class NavBar extends Component {
   render() {
     return (
       <div>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/recipes">Recipes</NavLink>
-            <NavLink to="/users">Users</NavLink>
+            <Nav>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/recipes">Recipes</NavLink>
+                <NavLink to="/users">Users</NavLink>
+            </Nav>
       </div>
     )
   }
