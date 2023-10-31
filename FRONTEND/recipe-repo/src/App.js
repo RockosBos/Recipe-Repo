994c17e4ce935c206b90b49d2fe1f12b0f585d19
RockosBos <32206608+RockosBos@users.noreply.github.com>
2023-10-31 17:58:22 -0400

    Login implementation

--- FRONTEND/recipe-repo/src/App.js
+++ FRONTEND/recipe-repo/src/App.js
@@ -14,11 +14,11 @@ import Login from "./pages/Login";
 
 
 function App() {
-    const [token, setToken] = useState();
+    /*const [token, setToken] = useState();
 
     if(!token) {
       return <Login setToken={setToken} />
-    }
+    }*/
 
     return (
         <Router>
