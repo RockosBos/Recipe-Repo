092c07db7ce33986aeecadac7c10e4da6e16fac6
RockosBos <32206608+RockosBos@users.noreply.github.com>
2023-12-10 00:59:31 -0500

    Frontend OPENAI page updated

--- FRONTEND/recipe-repo/src/pages/OpenAI.js
+++ FRONTEND/recipe-repo/src/pages/OpenAI.js
@@ -3,21 +3,23 @@ import './OpenAI.css';
 
 const DUMMY_AI_RESPONSE = "This is a test Response";
 
+const onSubmit = () => {
 
+}
 
 export default function OpenAI({setToken}) {
 	
 	return (
 		<div className="openAI-page">
-			<form>
-				<div className='contact-title'>
-					<label className='promptBoxLabel' htmlFor="promptBox">Enter OpenAI Prompt</label>
-					<input id="promptBox" className='promptBox'/>
+			<form className='open-ai-form' onSubmit={onSubmit}>
+			<label className='promptBoxLabel' htmlFor="promptBox">Enter OpenAI Prompt: </label>
+				<div className='prompt'>
+					<textarea id="promptBox" className='promptBox' rows="10" cols="50"/>
 				</div>
 				<button className='buttonElement'>Submit Prompt</button>
 			</form>
 
-			<p>{DUMMY_AI_RESPONSE}</p>
+			<p className='response'>{DUMMY_AI_RESPONSE}</p>
 
 		</div>
 	)
